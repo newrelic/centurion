83bc0934c61fa76e8d099535f534000f18dfd9a0
Didip Kerabat <didip@newrelic.com>
2014-09-29 09:39:17 -0700

    Check if dogestry command line exist and add logging.

--- lib/centurion/dogestry.rb
+++ lib/centurion/dogestry.rb
@@ -1,8 +1,33 @@
+require_relative 'logging'
+
 module Centurion; end
 
 class Centurion::Dogestry
+  include Centurion::Logging
+
   def initialize(options = {})
     @options = options
+  end
+
+  # Cross-platform way of finding an executable in the $PATH.
+  #   which('ruby') #=> /usr/bin/ruby
+  def which(cmd)
+    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exts.each { |ext|
+        exe = File.join(path, "#{cmd}#{ext}")
+        return exe if File.executable?(exe) && !File.directory?(exe)
+      }
+    end
+    return nil
+  end
+
+  def validate_before_exec
+    unless which('dogestry')
+      message = 'Unable to find "dogestry" executable'
+      error message
+      raise message
+    end
   end
 
   def aws_access_key
@@ -25,15 +50,35 @@ class Centurion::Dogestry
     "s3://#{s3_bucket}/?region=#{s3_region}"
   end
 
+  def docker_host
+    @options[:docker_host] || 'tcp://localhost:2375'
+  end
+
+  def set_envs
+    ENV['DOCKER_HOST'] = docker_host
+    ENV['AWS_ACCESS_KEY'] = aws_access_key
+    ENV['AWS_SECRET_KEY'] = aws_secret_key
+  end
+
   def exec_command(command, repo)
     "dogestry #{command} #{s3_url} #{repo}"
   end
 
   def pull(repo)
-    `#{exec_command('pull', repo)}`
+    validate_before_exec
+    set_envs
+
+    output = `#{exec_command('pull', repo)}`
+    info output
+    output
   end
 
   def push(repo)
-    `#{exec_command('push', repo)}`
+    validate_before_exec
+    set_envs
+
+    output = `#{exec_command('push', repo)}`
+    info output
+    output
   end
 end
