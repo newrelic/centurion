35866241a5994f5b52c34bcce8bdd6bbf6751fb9
Karl Matthias <kmatthias@newrelic.com>
2014-10-15 13:57:33 -0700

    Flesh out more tests around registry URL handling.

--- spec/docker_registry_spec.rb
+++ spec/docker_registry_spec.rb
@@ -3,57 +3,78 @@ require 'centurion/docker_registry'
 
 describe Centurion::DockerRegistry do
   let(:registry_url) { 'http://localhost/' }
-  let(:registry) { Centurion::DockerRegistry.new(registry_url) }
+  let(:registry)     { Centurion::DockerRegistry.new(registry_url) }
 
   describe '#repository_tags' do
     let(:repository) { 'foobar' }
-    let(:tag_name) { 'arbitrary_tag' }
-    let(:image_id) { 'deadbeef0000' }
-    let(:url) { any_args() }
+    let(:tag_name)   { 'arbitrary_tag' }
+    let(:image_id)   { 'deadbeef0000' }
+    let(:url)        { any_args() }
 
     before do
-      expect(Excon).to receive(:get).
-                       with(url).
-                       and_return(double(status: 200, body: response))
+      expect(Excon).to receive(:get).with(url).and_return(
+        double(status: 200, body: response)
+      )
     end
 
     subject { registry.repository_tags(repository) }
 
-    context 'when given a response from the official Docker registry' do
-      let(:registry_url) { Centurion::DockerRegistry::OFFICIAL_URL }
-      let(:response) { <<-JSON.strip }
-        [{"layer": "#{image_id}", "name": "#{tag_name}"}]
-      JSON
+    describe 'handling different responses from open source and official registries' do
+      context 'when given a response from the official Docker registry' do
+        let(:registry_url) { Centurion::DockerRegistry::OFFICIAL_URL }
+        let(:response)     { <<-JSON.strip }
+          [{"layer": "#{image_id}", "name": "#{tag_name}"}]
+        JSON
 
-      it 'normalizes the response' do
-        expect(subject).to eq(tag_name => image_id)
+        it 'normalizes the response' do
+          expect(subject).to eq(tag_name => image_id)
+        end
+      end
+
+      context 'when given a response from the open-source Docker registry' do
+        let(:response) { <<-JSON.strip }
+          {"#{tag_name}": "#{image_id}"}
+        JSON
+
+        it 'normalizes the response' do
+          expect(subject).to eq(tag_name => image_id)
+        end
       end
     end
 
-    context 'when given a response from the open-source Docker registry' do
-      let(:response) { <<-JSON.strip }
-        {"#{tag_name}": "#{image_id}"}
-      JSON
-
-      it 'normalizes the response' do
-        expect(subject).to eq(tag_name => image_id)
-      end
-    end
-
-    context 'when given the official Docker registry and a repository with a host name' do
+    context 'when given the official Docker registry and a repository with a hostname' do
       let(:registry_url) { Centurion::DockerRegistry::OFFICIAL_URL }
-      let(:repository) { 'docker-reg.example.com/foobar' }
-
-      let(:response) { <<-JSON.strip }
+      let(:repository)   { 'docker-reg.example.com/foobar' }
+      let(:response)     { <<-JSON.strip }
         {"#{tag_name}": "#{image_id}"}
       JSON
-
-      let(:url) { 'https://docker-reg.example.com/v1/repositories/foobar/tags' }
 
       it 'fetches from the image-referenced registry' do
         expect(subject).to eq(tag_name => image_id)
       end
     end
-  end
 
+    context 'when given any other registry' do
+      let(:registry_url) { 'http://my-registry.example.com' }
+      let(:response)     { <<-JSON.strip }
+        {"#{tag_name}": "#{image_id}"}
+      JSON
+
+      context 'and a repository with a hostname' do
+        let(:repository) { 'docker-reg.example.com/foobar' }
+
+        it 'fetches from the image-referenced registry' do
+          expect(subject).to eq(tag_name => image_id)
+        end
+      end
+
+      context 'and a repository with no hostname' do
+        let(:repository) { 'foobar' }
+
+        it 'fetches from the image-referenced registry' do
+          expect(subject).to eq(tag_name => image_id)
+        end
+      end
+    end
+  end
 end
