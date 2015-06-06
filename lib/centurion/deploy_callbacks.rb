b51f31387251bac0face4efb1bf76bb64aedd1f4
Mark Borcherding <markborcherding@gmail.com>
2015-06-05 21:21:47 -0500

    add after starting callbacks

--- lib/centurion/deploy_callbacks.rb
+++ lib/centurion/deploy_callbacks.rb
@@ -2,17 +2,28 @@ module Centurion
   # Callbacks to allow hooking into the deploy lifecycle. This could
   # be useful to communicate with a loadbalancer, chat room, etc.
   module DeployCallbacks
-    def stop_containers(target_server, service, timeout = 30)
+    def stop_containers(server, service, timeout = 30)
       before_stopping_container_callbacks.each do |callback|
-        callback.call target_server
+        callback.call server
       end
-      super target_server, service, timeout
+      super server, service, timeout
+    end
+
+    def start_new_container(server, service, restart_policy)
+      super server, service, restart_policy
+      after_new_container_started_callbacks.each do |callback|
+        callback.call server
+      end
     end
 
     private
 
     def before_stopping_container_callbacks
       fetch :before_stopping_container_callbacks, []
+    end
+
+    def after_new_container_started_callbacks
+      fetch :after_new_container_started_callbacks, []
     end
   end
 end
--- spec/deploy_callbacks_spec.rb
+++ spec/deploy_callbacks_spec.rb
@@ -2,16 +2,24 @@ require 'spec_helper'
 require 'centurion'
 
 RSpec.describe Centurion::DeployCallbacks do
-  describe 'before stopping callback' do
-    let(:callback) { [double, double] }
-    let(:klass) do
-      Class.new do
-        prepend Centurion::DeployCallbacks
-        def stop_containers(server, service, timeout)
-          doing_it_now server, service, timeout
-        end
+  let(:server) { double :server }
+  let(:service) { double :service }
+  let(:callbacks) { [double, double] }
+
+  let(:klass) do
+    Class.new do
+      prepend Centurion::DeployCallbacks
+      def stop_containers(server, service, timeout)
+        stopping_it_now server, service, timeout
+      end
+
+      def start_new_container(server, service, restart_policy)
+        starting_it_now server, service, restart_policy
       end
     end
+  end
+
+  describe 'before stopping callback' do
     let(:object) do
       klass.new.tap do |o|
         allow(o).to receive(:fetch)
@@ -19,22 +27,41 @@ RSpec.describe Centurion::DeployCallbacks do
           .and_return callbacks
       end
     end
-    let(:server) { double :server }
-    let(:service) { double :service }
     let(:timeout) { double :timeout }
-    let(:callbacks) { [double, double] }
-
     it 'invokes all the callback before stopping the container' do
       callbacks.each do |callback|
         expect(callback).to receive(:call)
           .with(server)
           .ordered
       end
-      expect(object).to receive(:doing_it_now)
+      expect(object).to receive(:stopping_it_now)
         .with(server, service, timeout)
         .ordered
 
       object.stop_containers server, service, timeout
+    end
+  end
+
+  describe 'after started callback' do
+    let(:object) do
+      klass.new.tap do |o|
+        allow(o).to receive(:fetch)
+          .with(:after_new_container_started_callbacks, [])
+          .and_return callbacks
+      end
+    end
+    let(:restart_policy) { double }
+    it 'invokes all the callbacks after the container is started' do
+      expect(object).to receive(:starting_it_now)
+        .with(server, service, restart_policy)
+        .ordered
+      callbacks.each do |callback|
+        expect(callback).to receive(:call)
+          .with(server)
+          .ordered
+      end
+
+      object.start_new_container server, service, restart_policy
     end
   end
 end
