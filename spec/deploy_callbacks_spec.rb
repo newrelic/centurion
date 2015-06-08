dfb7a24dc1fa23bd4f1b467a687d5e802f7a47f2
Mark Borcherding <markborcherding@gmail.com>
2015-06-07 20:45:33 -0500

    create dry callback test

--- spec/deploy_callbacks_spec.rb
+++ spec/deploy_callbacks_spec.rb
@@ -2,66 +2,59 @@ require 'spec_helper'
 require 'centurion'
 
 RSpec.describe Centurion::DeployCallbacks do
-  let(:server) { double :server }
-  let(:service) { double :service }
-  let(:callbacks) { [double, double] }
+  shared_examples_for 'a callback' do |callback|
+    let(:server) { double :server }
+    let(:service) { double :service }
+    let(:callbacks) { [double, double] }
+    let(:callback_name) { "#{callback}_callbacks".to_sym }
 
-  let(:klass) do
-    Class.new do
-      prepend Centurion::DeployCallbacks
-      def stop_containers(server, service, timeout)
-        stopping_it_now server, service, timeout
+    let(:klass) do
+      Class.new do
+        include Centurion::DeployCallbacks
+        def method_missing(method_name, *_args)
+          doing method_name
+        end
       end
+    end
 
-      def start_new_container(server, service, restart_policy)
-        starting_it_now server, service, restart_policy
+    let(:object) do
+      klass.new.tap do |o|
+        allow(o).to receive(:fetch).with(callback_name, []).and_return callbacks
       end
+    end
+  end
+
+  shared_examples_for 'a before callback' do |callback, method_name|
+    include_examples 'a callback', callback
+
+    it 'invokes all the callback before the method' do
+      callbacks.each { |cb| expect(cb).to receive(:call).with(server).ordered }
+      expect(object).to receive(:doing).with(method_name).ordered
+      subject
+    end
+  end
+
+  shared_examples_for 'an after callback' do |callback, method_name|
+    include_examples 'a callback', callback
+
+    it 'invokes all the callback before the method' do
+      expect(object).to receive(:doing).with(method_name).ordered
+      callbacks.each { |cb| expect(cb).to receive(:call).with(server).ordered }
+      subject
     end
   end
 
   describe 'before stopping callback' do
-    let(:object) do
-      klass.new.tap do |o|
-        allow(o).to receive(:fetch)
-          .with(:before_stopping_image_callbacks, [])
-          .and_return callbacks
-      end
-    end
-    let(:timeout) { double :timeout }
-    it 'invokes all the callback before stopping the container' do
-      callbacks.each do |callback|
-        expect(callback).to receive(:call)
-          .with(server)
-          .ordered
-      end
-      expect(object).to receive(:stopping_it_now)
-        .with(server, service, timeout)
-        .ordered
-
-      object.stop_containers server, service, timeout
-    end
+    subject { object.stop_containers server, service }
+    it_behaves_like 'a before callback',
+                    :before_stopping_image,
+                    :stop_containers
   end
 
   describe 'after started callback' do
-    let(:object) do
-      klass.new.tap do |o|
-        allow(o).to receive(:fetch)
-          .with(:after_image_started_callbacks, [])
-          .and_return callbacks
-      end
-    end
-    let(:restart_policy) { double }
-    it 'invokes all the callbacks after the container is started' do
-      expect(object).to receive(:starting_it_now)
-        .with(server, service, restart_policy)
-        .ordered
-      callbacks.each do |callback|
-        expect(callback).to receive(:call)
-          .with(server)
-          .ordered
-      end
-
-      object.start_new_container server, service, restart_policy
-    end
+    subject { object.start_new_container server, service, double }
+    it_behaves_like 'an after callback',
+                    :after_image_started,
+                    :start_new_container
   end
 end
