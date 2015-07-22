208155b8624dc384ef72498b6025e86ce3bfef24
Mark Borcherding <markborcherding@gmail.com>
2015-07-21 21:50:00 -0500

    Emit events in a more generic manner

--- lib/centurion/deploy_callbacks.rb
+++ lib/centurion/deploy_callbacks.rb
@@ -3,40 +3,35 @@ module Centurion
   # be useful to communicate with a loadbalancer, chat room, etc.
   module DeployCallbacks
     def stop_containers(server, service, timeout = 30)
-      callbacks(:before_stopping_image).each do |callback|
-        callback.call server
-      end
+      emit :before_stopping_image, server
       super server, service, timeout
     end
 
     def start_new_container(server, service, restart_policy)
-      result = super server, service, restart_policy
-      callbacks(:after_image_started).each do |callback|
-        callback.call server
-      end
-      result
+      super(server, service, restart_policy).tap { emit :after_image_started, server }
     end
 
     def wait_for_health_check_ok(health_check_method, server, port, endpoint, image_id, tag, sleep_time=5, retries=12)
-      result = super health_check_method,
-                     server,
-                     port,
-                     endpoint,
-                     image_id,
-                     tag,
-                     sleep_time,
-                     retries
-
-      callbacks(:after_health_check_ok).each do |callback|
-        callback.call server
-      end
-      result
+      super(health_check_method,
+            server,
+            port,
+            endpoint,
+            image_id,
+            tag,
+            sleep_time,
+            retries).tap { emit :after_health_check_ok, server }
     end
 
     private
 
-    def callbacks(name)
-      fetch "#{name}_callbacks".to_sym, []
+    def emit(name, *args)
+      callbacks[name].each do |callback|
+        callback.call(*args)
+      end
+    end
+
+    def callbacks
+      fetch 'callbacks', Hash.new { [] }
     end
   end
 end
