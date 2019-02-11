bc347b5988a16ba20125e5887c86b8c4880c2b07
Ben Weintraub <ben@newrelic.com>
2019-02-10 21:54:14 -0800

    Check permissions on remote Docker socket before forwarding it

--- lib/centurion/ssh.rb
+++ lib/centurion/ssh.rb
@@ -7,6 +7,8 @@ module Centurion; end
 module Centurion::SSH
   extend self
 
+  DOCKER_SOCKET_PATH = '/var/run/docker.sock'
+
   def with_docker_socket(hostname, user, log_level = nil)
     log_level ||= Logger::WARN
 
@@ -15,10 +17,16 @@ module Centurion::SSH
         ssh.logger = Logger.new STDERR
         ssh.logger.level = log_level
 
+        # Validate that we have access to the Docker socket before attempting to
+        # forward it. This ensures a meaningful error message if we don't.
+        if ssh.exec!("test -w '#{DOCKER_SOCKET_PATH}'").exitstatus != 0
+          raise "Docker socket at '#{DOCKER_SOCKET_PATH}' was not writable by user '#{user}' on '#{hostname}'. Is this user in the 'docker' group?"
+        end
+
         # Tempfile ensures permissions are 0600
         local_socket_path_file = Tempfile.new('docker_forward')
         local_socket_path = local_socket_path_file.path
-        ssh.forward.local_socket(local_socket_path, '/var/run/docker.sock')
+        ssh.forward.local_socket(local_socket_path, DOCKER_SOCKET_PATH)
 
         t = Thread.new do
           yield local_socket_path
